5c0cf05b3f94622a9a83b0fa3684853cfd21c7e6
philip <philip.oduffy@ul.ie>
2013-07-26 15:23:26 +0100

    routes working and added database

--- src/routing.py
+++ src/routing.py
@@ -1,10 +1,28 @@
-import bottle
-from bottle import route, run
+from bottle import route, run, template, request
 
-@route('/', method='GET')
-def homepage():
-    return 'Hello world!'
-    
-@route('/events/:id', method='GET')
+## Sends document to the db
+@route('/document', method='POST')
+def createDoc():
+    document = request.body
+    return document
+
+## Returns all documents on the db
+@route('/document', method='GET')
+def displayDocs():
+    return "displaying all documents on db"
+
+## Returns specified document on the db
+@route('/document/:id', method='GET')
 def get_event(id):
-    return dict(name = 'Event ' + str(id))
+    return "displaying document " + id
+    
+
+@route('/')
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
+
+ 
+run(host='localhost', port=8080, debug=True)
+
+
